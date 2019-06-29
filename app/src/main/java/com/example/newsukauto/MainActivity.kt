2c8b758ea9c97ecb77585ad05a4dedd5207940f3
zloper <makiavel@mail.ru>
2019-06-30 00:58:02 +0300

    add async

--- app/src/main/java/com/example/newsukauto/MainActivity.kt
+++ app/src/main/java/com/example/newsukauto/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.newsukauto
 import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.newsukauto.ui.login.LoginActivity
 
+import kotlinx.coroutines.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_set_url.*
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,8 +47,38 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        GlobalScope.launch { // launch a new coroutine in background and continue
+            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+//            val res = URL(urlLabel.text.toString() + "/monitor/status").readText()
+            val res = URL("https://www.google.com/").readText()
+            println("dbg_start!") // print after delay
+            println(res)
+            println("dbg_end")
+        }
+//
+//        suspend fun readUrl() = coroutineScope {
+//            val task1 = async { fetchResult(id = 42) }
+//            val task2 = async { fetchResult(id = 99) }
+//
+//            val results = awaitAll(task1, task2)
+//
+//            log(results)
+//        }
 
 
+//        suspend fun readUrl(urlAdress: String) {
+////            val res =  async { URL(urlLabel.text.toString() + "/monitor/status").readText() }.await
+//            try {
+//                var res = ""
+//                res = async {  URL(urlAdress).readText() }.await
+//                Log.i("[RES]", res.toString())
+//            } catch (ex: Exception) {
+//
+//            }
+//
+//        }
+//
+//        readUrl(urlLabel.text.toString() + "/monitor/status")
 
         resetBtn.setOnClickListener {
             env.edit().putString("URL", "").apply()
